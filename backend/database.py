315bf141aa56b0c25368968f6b7b7e8b18291322
schwa <itkw456@gmail.com>
2025-07-22 23:37:59 +0900

    revise databases for sqlalchemy 2.0. and need to add blood test table as in mermaid

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./cat_health.db"
 
@@ -9,7 +9,8 @@ engine = create_engine(
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+class Base(DeclarativeBase):
+    pass
 
 def get_db():
     db = SessionLocal()
--- backend/db_models.py
+++ backend/db_models.py
@@ -1,52 +1,107 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Float, JSON
-from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey, DateTime
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from .database import Base
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "user"
 
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
-    is_active = Column(Boolean, default=True)
-    verified = Column(Boolean, default=False)
-    nickname = Column(String, index=True)
-    phone = Column(String, index=True)
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    username: Mapped[str] = mapped_column(unique=True, index=True)
+    email: Mapped[str] = mapped_column(unique=True, index=True)
+    hashed_password: Mapped[str]
+    is_active: Mapped[bool] = mapped_column(default=True)
+    verified: Mapped[bool] = mapped_column(default=False)
+    nickname: Mapped[str] = mapped_column(index=True)
+    phone: Mapped[str] = mapped_column
 
-    cats = relationship("Cat", back_populates="owner")
+    cats = relationship("Cat", back_populates="owns")
 
 class Cat(Base):
-    __tablename__ = "cats"
+    __tablename__ = "cat"
 
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, index=True)
-    owner_id = Column(Integer, ForeignKey("users.id"))
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    owner_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    name: Mapped[str] = mapped_column(index=True)
+    species: Mapped[str] = mapped_column(index=True)
+    birth_date: Mapped[DateTime] = mapped_column(index=True)
+    age: Mapped[int] = mapped_column(index=True)
+    gender: Mapped[str] = mapped_column(index=True)
+    neutered: Mapped[bool] = mapped_column(index=False)
 
-    owner = relationship("User", back_populates="cats")
-    health_records = relationship("HealthRecord", back_populates="cat")
-    blood_tests = relationship("BloodTest", back_populates="cat")
+    owner: Mapped["User"] = relationship(back_populates="cat")
+    health_record: Mapped["HealthRecord"] = relationship(back_populates="cat")
+    blood_test: Mapped["BloodTestEvent"] = relationship(back_populates="cat")
 
 class HealthRecord(Base):
-    __tablename__ = "health_records"
+    __tablename__ = "health_record"
 
-    id = Column(Integer, primary_key=True, index=True)
-    date = Column(String)
-    weight = Column(Float)
-    meals = Column(Integer)
-    poops = Column(Integer)
-    plays = Column(Integer)
-    sleeps = Column(Integer)
-    cat_id = Column(Integer, ForeignKey("cats.id"))
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    date: Mapped[DateTime] = mapped_column(index=True)
+    weight: Mapped[float] = mapped_column(index=True)
+    meals: Mapped[int] = mapped_column(index=True)
+    water: Mapped[int] = mapped_column(index=True)
+    urine: Mapped[int] = mapped_column(index=True)
+    poops: Mapped[int] = mapped_column(index=True)
+    plays: Mapped[int] = mapped_column(index=True)
+    sleeps: Mapped[int] = mapped_column(index=True)
+    notes: Mapped[str] = mapped_column(index=True)
+    cat_id: Mapped[int] = mapped_column(ForeignKey("cat.id"))
 
-    cat = relationship("Cat", back_populates="health_records")
+    cat: Mapped["Cat"] = relationship(back_populates="health_record")
 
-class BloodTest(Base):
-    __tablename__ = "blood_tests"
+class BloodTestEvent(Base):
+    __tablename__ = "blood_test_event"
 
-    id = Column(Integer, primary_key=True, index=True)
-    date = Column(String)
-    results = Column(JSON)
-    cat_id = Column(Integer, ForeignKey("cats.id"))
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    date: Mapped[DateTime] = mapped_column(index=True)
+    hospital: Mapped[str] = mapped_column(index=True)
+    notes: Mapped[str] = mapped_column(index=True)
 
-    cat = relationship("Cat", back_populates="blood_tests")
+    cat_id: Mapped[int] = mapped_column(ForeignKey("cat.id"))
+
+    cat: Mapped["Cat"] = relationship(back_populates="blood_test_event")
+
+class CBCPanel(Base):
+    __tablename__ = "cbc_panel"
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    event_id: Mapped[int] = mapped_column(ForeignKey("blood_test_event.id"), index=True)
+    wbc: Mapped[float] = mapped_column(index=True)
+    rbc: Mapped[float] = mapped_column(index=True)
+    hgb: Mapped[float] = mapped_column(index=True)
+    hct: Mapped[float] = mapped_column(index=True)
+    plt: Mapped[float] = mapped_column(index=True)
+
+    event: Mapped["BloodTestEvent"] = relationship(back_populates="cbc_panel")
+
+class ChemistryPanel(Base):
+    __tablename__ = "chemistry_panel"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    event_id: Mapped[int] = mapped_column(ForeignKey("blood_test_event.id"), index=True)
+    alp: Mapped[float] = mapped_column(index=True)
+    alt: Mapped[float] = mapped_column(index=True)
+    bun: Mapped[float] = mapped_column(index=True)
+    crea: Mapped[float] = mapped_column(index=True)
+
+    event: Mapped["BloodTestEvent"] = relationship(back_populates="chemistry_panel")
+
+class LiverPanel(Base):
+    __tablename__ = "liver_panel"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    event_id: Mapped[int] = mapped_column(ForeignKey("blood_test_event.id"), index=True)
+    ast: Mapped[float] = mapped_column(index=True)
+    ggt: Mapped[float] = mapped_column(index=True)
+    tbli: Mapped[float] = mapped_column(index=True)
+
+    event: Mapped["BloodTestEvent"] = relationship(back_populates="liver_panel")
+
+class DiabetesPanel(Base):
+    __tablename__ = "diabetes_panel"
+
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    event_id: Mapped[int] = mapped_column(ForeignKey("blood_test_event.id"), index=True)
+    glu: Mapped[float] = mapped_column(index=True)
+    fru: Mapped[float] = mapped_column(index=True)
+
+    event: Mapped["BloodTestEvent"] = relationship(back_populates="diabetes_panel")
